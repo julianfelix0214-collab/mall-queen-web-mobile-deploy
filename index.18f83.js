15918d0145114fadaa3904fe7457baa9cf8d4a2c
Julian <julian@Julians-MacBook-Air.local>
2026-07-07 17:43:50 +0800

    时装系统 M3：小星六套真立绘替换占位美术

--- index.18f83.js
+++ index.18f83.js
@@ -1,4 +1,4 @@
-System.register(["./application.aec22.js"], function (_export, _context) {
+System.register(["./application.32d8b.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
