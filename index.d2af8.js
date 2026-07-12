3aaaa0bf73ecd1ce6ea831c30e6a1a567e8488b8
Julian <julian@Julians-MacBook-Air.local>
2026-07-12 21:56:33 +0800

    hotfix: MallView 补 MALL_COPY/uiColor 缺失 import 修复主视图崩溃

--- index.d2af8.js
+++ index.d2af8.js
@@ -1,4 +1,4 @@
-System.register(["./application.32d8b.js"], function (_export, _context) {
+System.register(["./application.23a65.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
